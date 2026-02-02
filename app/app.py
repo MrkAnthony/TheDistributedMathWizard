9e5d13ab40d6a6367a93dce4ae21b20726563b5e
MrkAnthony <markadelva@yahoo.com>
2026-02-02 18:16:54 -0500

    Added rate-limiting with Valkey

--- app/app.py
+++ app/app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, jsonify
 import multiprocessing
 import os
-
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 from app.controllers.identity_controller import identity_handler
 from app.controllers.math_controller import add_handler
 from app.controllers.task_controller import create_task_handler, check_status_handler
@@ -11,6 +12,13 @@ from app.controllers.valkey_controller import valkey_test_handler
 from app.services.worker_service import run_worker_process
 
 app = Flask(__name__)
+# Uses the same Valkey Instance but they will coexist
+storage_uri = f"redis://{os.getenv('VALKEY_HOST', 'localhost')}:{os.getenv('VALKEY_PORT', '6379')}"
+limiter = Limiter(
+    get_remote_address,
+    app=app,
+    storage_uri=storage_uri
+)
 
 _worker = None  # module-level so we don’t spawn multiple
 
@@ -58,6 +66,10 @@ def handle_unexpected_error(err):
     }), 500
 
 
+@app.errorhandler(429)
+def handle_too_many_request(e):
+    return "", 429
+
 @app.route('/')
 def hello_world():
     return render_template('index.html')
@@ -79,6 +91,7 @@ def check_status(task_id):
 
 
 @app.route('/task', methods=['POST'])
+@limiter.limit("100 per minute")
 def create_task():
     return create_task_handler()
 
@@ -90,7 +103,6 @@ def valkey_test():
 
 # Start the worker once per container (works with `flask run` too)
 maybe_start_worker()
-
 
 if __name__ == "__main__":
     # If you run `python app/app.py`, Flask will start here
